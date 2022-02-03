eb83b18146e4164737f2e224391e0769b9b1c349
HelenaGiuffrida <helenadelbianco@hotmail.com>
2022-02-03 10:19:25 -0300

    first commit

---
+++ ContaCorrente.js
@@ -0,0 +1,28 @@
+export class ContaCorrente {
+    agencia;
+    cliente;
+
+
+    //# =0 https://github.com/tc39/proposal-class-fields#private-fields
+    #saldo = 0;
+   
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <=0) return;
+        
+        this.saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
+}
